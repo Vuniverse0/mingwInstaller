2ee70b2dcab0f36f3777371d24629486765b4292
Vuniverse0 <v.universe@protonmail.com>
2022-09-25 15:43:21 +0300

    fl/fl

--- src/pages.hpp
+++ src/pages.hpp
@@ -11,6 +11,7 @@ constexpr std::size_t button_height = 25;
 constexpr std::size_t button_x = 540;
 constexpr std::size_t button_y = 280;
 
+#include <FL/Fl.H>
 #include <Fl/Fl_Widget.H>
 
 void next_cb(Fl_Widget*, void*);
