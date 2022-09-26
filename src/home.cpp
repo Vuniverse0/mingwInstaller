a7866448389351cfe43acaf83bb6f1385b5c7be4
Vuniverse0 <v.universe@protonmail.com>
2022-09-26 18:19:13 +0300

    shlobj

--- src/home.cpp
+++ src/home.cpp
@@ -8,11 +8,15 @@
 
 std::string home()
 {
-    LPSTR path[MAX_PATH];
-    if(SUCCEEDED(SHGetFolderPathA(NULL, CSIDL_PROFILE, NULL, 0, path)))
-        return path;
-    else
+    LPSTR path = new char[MAX_PATH];
+    if(SUCCEEDED(SHGetFolderPathA(NULL, CSIDL_PROFILE, NULL, 0, path))){
+        std::string res {path};
+        delete path;
+        return res;
+    }else{
+        delete path;
         throw std::runtime_error("cant get home path");
+    }
 }
 
 #else
