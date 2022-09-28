c6b4dd94f400b3cd5a3c30d42f57509ffcc9d34e
Vuniverse0 <v.universe@protonmail.com>
2022-09-28 13:04:13 +0300

    try link

--- src/shortcut.cpp
+++ src/shortcut.cpp
@@ -11,8 +11,7 @@
 #include "shlguid.h"
 #include "shobjidl.h"
 #include <cwchar>
-
-std::wstring getProgramsFolder();
+#include <KnownFolders.h>
 
 /*============================================================================*/
 
@@ -26,66 +25,14 @@ std::wstring getProgramsFolder();
 //                including the file name.
 // lpszPathLink - Address of a buffer that contains the path where the
 //                Shell link is to be stored, including the file name.
-// lpszPath     - Working directory of target Obj file // Mingw?
 // lpszDesc     - Address of a buffer that contains a description of the
 //                Shell link, stored in the Comment field of the link
 //                properties.
+//lpszPicture   - picture of shortcut
 
-    HRESULT                     CreateLink(
-    LPCSTR                      lpszPathObj,
-    LPCWSTR                      lpszPathLink,
-    LPCSTR                      lpszPath,
-    LPCSTR                      lpszDesc  = "Description") //TOTO HERE !!! ADAPTATE
-
-/*============================================================================*/
+HRESULT CreateLink(LPCSTR lpszPathObj, LPCWSTR lpszPathLink,
+                   LPCSTR lpszDesc, LPCSTR lpszPicture)
 {
-    /*IShellLink* psl = NULL;
-    HRESULT hres = CoInitialize(NULL);
-
-    if (!SUCCEEDED(hres))
-        printf("Eror 46 chortcut");
-    // Get a pointer to the IShellLink interface. It is assumed that CoInitialize
-    // has already been called.
-
-    hres = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (LPVOID*)&psl);
-    if (SUCCEEDED(hres))
-    {
-        IPersistFile* ppf;
-
-        // Set the path to the shortcut target and add the description.
-        psl->SetPath(lpszPathObj);
-        psl->SetDescription(lpszDesc);
-        psl->SetWorkingDirectory(lpszPath);
-        //psl->SetIconLocation(lpszPathObj, 0);
-
-        // Query IShellLink for the IPersistFile interface, used for saving the
-        // shortcut in persistent storage.
-        hres = psl->QueryInterface(IID_IPersistFile, (LPVOID*)&ppf);
-
-        if (SUCCEEDED(hres))
-        {
-            WCHAR wsz[MAX_PATH];
-
-            // Ensure that the string is Unicode.
-            MultiByteToWideChar(CP_ACP, 0, lpszPathLink, -1, wsz, MAX_PATH);
-
-            // Add code here to check return value from MultiByteWideChar
-            // for success.
-
-            // Save the link by calling IPersistFile::Save.
-            hres = ppf->Save(wsz, TRUE);
-            if (!SUCCEEDED(hres))
-                printf("Eror 77 chortcut : %d", hres);
-
-            ppf->Release();
-        }
-        psl->Release();
-    }
-
-    CoUninitialize();
-
-    return hres; */
-
     CoInitializeEx( NULL, 0 );
     HRESULT hres = 0;
     IShellLink* psl;
@@ -103,7 +50,7 @@ std::wstring getProgramsFolder();
             // Set the path to the shortcut target and add the description.
             psl->SetPath(lpszPathObj);
             psl->SetDescription(lpszDesc);
-            //psl->SetIconLocation(mediaMaestroLocation, 0);
+            psl->SetIconLocation(lpszPicture, 0);
 
             // Query IShellLink for the IPersistFile interface, used for saving the
             // shortcut in persistent storage.
@@ -128,26 +75,33 @@ std::wstring getProgramsFolder();
     CoUninitialize();
     return hres;
 }
-void shortcut(const std::string& file,  const std::string& workPath, const std::string& description)
+void shortcut(const std::string& filePath, const std::string& description, const std::string& picturePath)
 {
     //std::wstring wfile = std::wstring(file.begin(), file.end());
     //std::wstring wdescription = std::wstring(description.begin(), description.end());
-    std::wstring location = getProgramsFolder();
-    //location+=L"\\mingw";
+    wchar_t* p = nullptr;
+    //HRESULT hres = SHGetKnownFolderPath(FOLDERID_Desktop, 0, NULL, &p);
+    HRESULT hres = SHGetKnownFolderPath(FOLDERID_CommonStartMenu, 0, NULL, &p);
+    std::wstring location = p;
+    //FOLDERID_CommonStartup
+    // FOLDERID_CommonPrograms
+    // FOLDERID_CommonStartMenu
 
+    //location+=L"\\mingw";
 
     using convert_type = std::codecvt_utf8<wchar_t>;
     std::wstring_convert<convert_type, wchar_t> converter;
     std::string l = converter.to_bytes( location );
 
-
-    std::string Filef = file;
-    std::string work = workPath;
+    std::string Filef = filePath, Picf = picturePath;
     std::replace(Filef.begin(), Filef.end(), '/', '\\');
-    //std::replace(work.begin(), work.end(), '\\', '/');
-    printf("\n\nWanna to create shortcut for:  %s,\n to:  %s,\n  work path: %s,\n description: %s\n\n",
-           Filef.c_str(), l.c_str(), work.c_str(), description.c_str());
-    CreateLink(Filef.c_str(), location.c_str(), work.c_str(), description.c_str());
+    std::replace(Picf.begin(), Picf.end(), '/', '\\');
+    //HRESULT CreateLink(LPCSTR lpszPathObj, LPCWSTR lpszPathLink, LPCSTR lpszDesc, LPCSTR lpszPicture);
+
+    printf("\n\nWanna to create shortcut for:  %s,\n to:  %s,\n  picture: %s,\n description: %s\n\n",
+           Filef.c_str(), l.c_str(), Picf.c_str(), description.c_str());
+
+    CreateLink(Filef.c_str(), location.c_str(), work.c_str(), description.c_str(), Picf.c_str());
 }
 
 #else
